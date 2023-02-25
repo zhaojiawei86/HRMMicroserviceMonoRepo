5155e9c79ed06748b06fb825e1c0408a3c1191a4
zhaojiawei86 <59852184+zhaojiawei86@users.noreply.github.com>
2023-02-24 22:36:52 -0500

    recruit database

---
+++ Hrm.Recruitment.APILayer/Program.cs
@@ -0,0 +1,34 @@
+﻿using Hrm.Recruitment.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var connectionString = builder.Configuration.GetConnectionString("HrmRecruitDb");
+var dockerConnStr = Environment.GetEnvironmentVariable("HrmRecruitDb");
+builder.Services.AddDbContext<RecruitDbContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+    //options.UseSqlServer(dockerConnStr);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
