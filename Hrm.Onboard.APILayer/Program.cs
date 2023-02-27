886b6ae6578d8397c14ebc5da0045e32342f55a6
zhaojiawei86 <59852184+zhaojiawei86@users.noreply.github.com>
2023-02-27 05:01:54 -0500

    add onboard entity

--- Hrm.Onboard.APILayer/Program.cs
+++ Hrm.Onboard.APILayer/Program.cs
@@ -1,4 +1,7 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Hrm.Onboard.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,6 +9,23 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("HrmOnboardDb");
+var dockerConnStr = Environment.GetEnvironmentVariable("HrmOnboardDbDocker");
+builder.Services.AddDbContext<OnboardDbContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+    //options.UseSqlServer(dockerConnStr);
+});
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
